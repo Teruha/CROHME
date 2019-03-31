c5e1baf224926bbaa61b1eaa66dea19639295cd6
qh <qh@qhs-MacBook-Pro.local>
2019-03-30 20:02:11 -0400

    Added file walking capabilities to the project file

--- CROHME.py
+++ CROHME.py
@@ -1,39 +1,8 @@
-from bs4 import BeautifulSoup
+import os
+import csv
 import matplotlib.pyplot as plt
 
-import csv
-
-# path to training data (locally)
-folderPath = '/Users/rachaelthormann/Desktop/Classes Spring 2019/Pattern Recognition/Project 1/'
-iso_4'/Users/qh/Documents/python_projects/csci-737/project-part-1/task2-trainSymb2014/trainingSymbols/iso4.inkml'
-
-# FEATURE POSSIBILITIES
-# number of trace points, normalized values ...
-
-"""
-def createInputCSV():
-
-    f = open(folderPath + 'InputTrainingSymbol.CSV','w')
-
-    for i in range(0, 85802):
-        f.write('iso' + str(i) + '.inkml\n')
-
-    f.close()
-
-    return
-"""
-for values in soup.findAll("trace"):
-    trace_groups = str(values.contents[0])
-    points = []
-    for i, coor in enumerate(trace_groups.replace('\n', '').split(',')):
-        x, y = coor.split()
-        x, y = int(x), -int(y)
-        points.append((x, y))
-#         plt.plot(x,y, 'ro')
-#         plt.annotate(i, xy=(x, y), xytext=(5, 4), ha='left', textcoords='offset points')
-    for i in range(len(points)-1):
-        plt.plot((points[i][0], points[i+1][0]), (points[i][1], points[i+1][1]), color='black')
-
+from bs4 import BeautifulSoup
 def draw_xml_file(trace_groups):
     """
     Draw the trace groups from a given XML file
@@ -72,38 +41,99 @@ def extract_num_points_and_strokes(trace_groups):
     for trace in trace_groups:
         trace_as_string = str(trace.contents[0])
         num_points += len(trace_as_string.replace('\n', '').split(','))
-    
+
     return num_points, num_strokes
 
-def readInputData(file):
-    with open(file) as csvfile:
-        readCSV = csv.reader(csvfile, delimiter=',')
-        i = 0
-        for row in readCSV:
-            if (i == 4):
-                print(row[0])
+def read_input_data(file):
+    """
+    Reads a single .inkML file
+    
 
-                with open(folderPath + 'task2-trainSymb2014/trainingSymbols/' + row[0], 'r') as f:
+    Parameters:
+    file (string) - file name to read from current directory
 
-                    soup = BeautifulSoup(f, features="lxml")
-                    # you can iterate nd get whatever tag <> is needed
-                    for node in soup.find_all('annotation'):
-                        print(node)
-                    
-                    draw_xml_file(soup.findAll("trace"))
-                    extract_num_points_and_strokes(soup.findAll("trace"))
-                    
+    Returns:
+    None
+    """
+    with open(file, 'r') as f:
+
+        soup = BeautifulSoup(f, features="lxml")
+        # you can iterate nd get whatever tag <> is needed
+        for node in soup.find_all('annotation'):
+            print(node)
+        
+        draw_xml_file(soup.findAll("trace"))
+        extract_num_points_and_strokes(soup.findAll("trace"))
+
+        plt.show()
 
 
-                    plt.show()
+def read_training_symbol_directory():
+    """
+    This function assumes that the training data is in a folder and that folder is within the same 
+    directory as this file. This allows us to use the "os" package to look for and load the training 
+    data
 
-            i += 1
+    ex.
+        ls dir
+            CROHME.py
+            task2-trainSymb2014
+        cd task2-trainSymb2014
+        ls 
+            trainingSymbols
+            trainingJunk
+            ...
+            etc.
+    Parameters:
+    None
 
-    return
+    Returns:
+    training_symbol_files (list) - List of the full paths of all training files
+    """
+    training_symbol_files = []
+    for (dirname, dirs, files) in os.walk(os.getcwd()):
+        if 'trainingSymbols' in dirname:
+            os.chdir(dirname)
+            for (dirname, dirs, files) in os.walk(os.getcwd()):
+                for f in files:
+                    training_symbol_files.append(dirname +'/'+ f)
+    return training_symbol_files
 
+def read_training_junk_directory():
+    """
+    This function assumes that the training data is in a folder and that folder is within the same 
+    directory as this file. This allows us to use the "os" package to look for and load the training 
+    data
+
+    ex.
+        ls dir
+            CROHME.py
+            task2-trainSymb2014
+        cd task2-trainSymb2014
+        ls 
+            trainingSymbols
+            trainingJunk
+            ...
+            etc.
+    Parameters:
+    None
+
+    Returns:
+    training_symbol_files (list) - List of the full paths of all junk files
+    """
+    training_junk_files = []
+    for (dirname, dirs, files) in os.walk(os.getcwd()):
+        if 'trainingJunk' in dirname:
+            os.chdir(dirname)
+            for (dirname, dirs, files) in os.walk(os.getcwd()):
+                for f in files:
+                    training_junk_files.append(dirname +'/'+ f)
+    return training_junk_files
 
 def main():
-    readInputData(folderPath + 'InputTrainingSymbol.CSV')
+    symbol_files = read_training_symbol_directory()
+    junk_files = read_training_junk_directory()
 
-if __name__ = '__main__':
+
+if __name__ == '__main__':
     main()
