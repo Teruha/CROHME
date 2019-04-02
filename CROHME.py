b7e4fcb0445e7233833984eefcc00408b8ec4b68
Qadir <Haqq.Q173@gmail.com>
2019-04-01 23:31:46 -0400

    Added support for my windows machine and a few other feature extractors

--- CROHME.py
+++ CROHME.py
@@ -2,10 +2,14 @@ import os
 import csv
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 from bs4 import BeautifulSoup
+from sys import platform
 
 EXCLUDED_FILES = ['iso_GT.txt', 'crohme_data']
+WINDOWS_PLATFORM = "win32"
+DEBUG = True # TODO: SET THIS TO FALSE BEFORE SUBMISSION
 
 def file_sorting_helper(full_path_name):
     """
@@ -17,10 +21,48 @@ def file_sorting_helper(full_path_name):
     Returns:
     iso_num (int) - the number of the iso file given in the full_path_name
     """
-    iso_name = full_path_name.split('/')[-1]
+    iso_name = None
+    if platform == WINDOWS_PLATFORM:
+        iso_name = full_path_name.split('\\')[-1]
+    else:
+        iso_name = full_path_name.split('/')[-1]
     file_extension_idx = iso_name.index('.')
     iso_num = int(iso_name[3:file_extension_idx])
     return iso_num
+
+def get_coordinates_from_trace(trace):
+    """
+    Get the coordinates from a single trace group
+
+    Parameters:
+    trace (string) - string of the coordinates separated by commas 
+
+    Returns:
+    points (list) - a list of tuples that represents the x,y coordinates
+    """
+    points = []
+    trace_as_string = str(trace.contents[0])
+    for coor in trace_as_string.replace('\n', '').split(','):
+        x, y = coor.split()[:2]
+        x, y = int(x), -int(y)
+        points.append((x, y))
+    return points
+
+def separate_x_y_coors_from_trace(trace):
+    """
+    Return the all the x_coordinate values and all the y_coordinate values respectively from the points
+
+    Parameters:
+    trace (string) - string of the coordinates separated by commas
+
+    Returns:
+    x_coors (list) - list of ints representing the x coordinate of their corresponding point
+    y_coors (list) - list of ints representing the y coordinate of their corresponding point
+    """
+    points = get_coordinates_from_trace(trace)
+    x_coors = [p[0] for p in points]
+    y_coors = [p[1] for p in points]
+    return x_coors, y_coors
 
 def draw_xml_file(trace_groups):
     """
@@ -33,12 +75,7 @@ def draw_xml_file(trace_groups):
     None
     """
     for trace in trace_groups:
-        trace_as_string = str(trace.contents[0])
-        points = []
-        for i, coor in enumerate(trace_as_string.replace('\n', '').split(',')):
-            x, y = coor.split()[:2]
-            x, y = int(x), -int(y)
-            points.append((x, y))
+        points = get_coordinates_from_trace(trace)
         # Draw line segments between points on the plot, 
         # to see points set the "marker" parameter to "+" or "o"
         for i in range(len(points)-1):
@@ -78,6 +115,33 @@ def extract_directions(trace_groups):
         3 - left
         4 - right
     """
+    up = 1
+    down = 2
+    left = 3
+    right = 4
+    # get the coordinates from the trace groups 
+    directions = []
+    for trace_idx, trace in enumerate(trace_groups):
+        directions_for_trace = []
+        x_coors, y_coors = separate_x_y_coors_from_trace(trace)
+        for i in range(1, len(x_coors)): # starting from 1 because we compare the previous point
+            if len(directions_for_trace) < 4:
+                # Up
+                if up not in directions_for_trace and (y_coors[i] - y_coors[i-1] > 0):
+                    directions_for_trace.append(up)
+                # Down
+                if down not in directions_for_trace and (y_coors[i] - y_coors[i-1] < 0):
+                    directions_for_trace.append(down)
+                # Left
+                if left not in directions_for_trace and (x_coors[i] - x_coors[i-1] < 0):
+                    directions_for_trace.append(left)
+                # Right
+                if right not in directions_for_trace and (x_coors[i] - x_coors[i-1] > 0):
+                    directions_for_trace.append(right)
+        if DEBUG:
+            print('Directions for stroke {0}: {1}'.format(trace_idx, directions_for_trace))
+        directions.extend(directions_for_trace)
+    return directions
 
 def extract_curvature(trace_groups):
     """
@@ -89,6 +153,30 @@ def extract_curvature(trace_groups):
     Returns:
     curvature (float) - The quantified curvature of a symbol
     """
+    character_curvature = []
+    for trace in trace_groups:
+        trace_curves = []
+        x_coors, y_coors = separate_x_y_coors_from_trace(trace)
+        for i in range(1, len(x_coors)):
+            delta_x = 0.01
+            delta_y = 0
+            if len(x_coors) > 4:
+                if i < 2:
+                    delta_x = x_coors[i + 2] - x_coors[i]
+                    delta_y = y_coors[i + 2] - y_coors[i]
+                elif i > len(x_coors) - 3:
+                    delta_x = x_coors[i] - x_coors[i - 2]
+                    delta_y = y_coors[i] - y_coors[i - 2]
+                else:
+                    delta_x = x_coors[i + 2] - x_coors[i - 2]
+                    delta_y = y_coors[i + 2] - y_coors[i - 2]
+                if delta_x != 0:
+                    trace_curves.append(np.arctan(delta_y / delta_x))
+        if len(trace_curves) == 0:
+            character_curvature.append(0)
+        else:
+            character_curvature.append(np.mean(trace_curves))
+    return np.mean(character_curvature)
 
 def extract_aspect_ratio(trace_groups):
     """
@@ -100,7 +188,25 @@ def extract_aspect_ratio(trace_groups):
     Returns:
     aspect_ratio (float) - aspect ratio of the trace groups
     """
+    max_x, min_x = -np.inf, np.inf
+    max_y, min_y = -np.inf, np.inf
+    for trace in trace_groups:
+        x_coors, y_coors = separate_x_y_coors_from_trace(trace)
+        max_x = np.amax(x_coors) if np.amax(x_coors) > max_x else max_x
+        max_y = np.amax(y_coors) if np.amax(y_coors) > max_y else max_y
+        min_x = np.amin(x_coors) if np.amin(x_coors) < min_x else min_x
+        min_y = np.amin(y_coors) if np.amin(y_coors) < min_y else min_y
 
+    width = max_x - min_x
+    height = max_y - min_y
+
+    if width <= 0:
+        width = 0.01
+    if height <= 0:
+        height = 0.01
+    return width / height
+
+# @Rachael do you think we need this?
 def normalize_drawing(trace_groups):
     """
     Normalizes points between -1 and 1 in a 2D space
@@ -122,6 +228,17 @@ def smooth_points(trace_groups):
     Returns:
     new_trace_groups (list) - smoothed trace_group points
     """
+    new_trace_groups = []
+    for trace in trace_groups:
+        new_trace = '<trace>'
+        points = get_coordinates_from_trace(trace)
+
+
+
+        new_trace += '<trace>'
+        new_trace_groups.append(new_trace)
+    
+
     
 def extract_features(file, draw_input_data=False):
     """
@@ -140,11 +257,17 @@ def extract_features(file, draw_input_data=False):
         for node in soup.findAll('annotation')[1]:
             unique_id = str(node)
         
-        if draw_input_data:
-            draw_xml_file(soup.findAll('trace'))
-        num_points, num_strokes = extract_num_points_and_strokes(soup.findAll('trace'))
+        trace_groups = soup.findAll('trace')
 
-    return {'UI': unique_id, 'NUM_POINTS': num_points, 'NUM_STROKES': num_strokes, 'SYMBOL_REPRESENTATION': None}
+        if draw_input_data:
+            draw_xml_file(trace_groups)
+        num_points, num_strokes = extract_num_points_and_strokes(trace_groups)
+        directions = extract_directions(trace_groups)
+        curvature = extract_curvature(trace_groups)
+        aspect_ratio = extract_aspect_ratio(trace_groups)
+
+    return {'UI': unique_id, 'NUM_POINTS': num_points, 'NUM_STROKES': num_strokes, 'DIRECTIONS': directions, 
+            'CURVATURE': curvature, 'ASPECT_RATIO': aspect_ratio, 'SYMBOL_REPRESENTATION': None}
 
 def read_training_symbol_directory():
     """
@@ -175,7 +298,10 @@ def read_training_symbol_directory():
             for (dirname, dirs, files) in os.walk(os.getcwd()):
                 for f in files:
                     if (f not in EXCLUDED_FILES) and ('iso' in f): # we want to ignore these files
-                        training_symbol_files.append(dirname +'/'+ f)
+                        if platform == WINDOWS_PLATFORM:
+                            training_symbol_files.append(dirname +'\\'+ f)
+                        else:
+                            training_symbol_files.append(dirname +'/'+ f)
     training_symbol_files.sort(key=lambda s: file_sorting_helper(s))
     return training_symbol_files
 
@@ -228,7 +354,10 @@ def map_ids_to_symbols():
             for (dirname, dirs, files) in os.walk(os.getcwd()):
                 for f in files:
                     if (f == 'iso_GT.txt'):
-                        ground_truth_file = dirname +'/'+ f
+                        if platform == WINDOWS_PLATFORM:
+                            ground_truth_file = dirname +'\\'+ f
+                        else:
+                            ground_truth_file = dirname +'/'+ f
                         break
                 if ground_truth_file != None:
                     break
@@ -251,14 +380,15 @@ def build_training_data(symbol_files, print_progress=True):
     Returns:
     data (Dataframe) - A pandas dataframe representation of the data
     """
-    df = pd.DataFrame(columns=['UI', 'NUM_POINTS', 'NUM_STROKES', 'SYMBOL_REPRESENTATION'])
+    df = pd.DataFrame(columns=['UI','NUM_POINTS','NUM_STROKES','DIRECTIONS','CURVATURE','ASPECT_RATIO', 'SYMBOL_REPRESENTATION'])
     ui_to_symbols = map_ids_to_symbols()
     num_files = len(symbol_files)
     for i, symbol_file in enumerate(symbol_files):
-        row = extract_features(symbol_file)
+        row = extract_features(symbol_file, True)
         row['SYMBOL_REPRESENTATION'] = ui_to_symbols[row['UI']]
         df.loc[i] = list(row.values())
-        if i % (num_files//100)==0 and print_progress:
+        percentage = num_files//100
+        if print_progress and percentage != 0 and i % percentage == 0:
             print('{0} ({1}%) of {2} files loaded...'.format(i, round((i/num_files)*100), num_files))
     print('Files 100% loaded.')
     return df # use this to operate on the data
@@ -268,9 +398,9 @@ def main():
     # TODO: Add df.to_pickle and df.read_pickle for saving and reading dataframe 
     # This way we won't have to read the training data everytime
     symbol_files = read_training_symbol_directory()
-    df = build_training_data(symbol_files[:1000], False)
-    print(df.head(5))
-    junk_files = read_training_junk_directory()
+    df = build_training_data(symbol_files[2:3], False)
+    print(df)
+    # junk_files = read_training_junk_directory()
 
 if __name__ == '__main__':
     main()
